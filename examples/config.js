5b7e81b015f6eabd960db32524e609acea255381
hudaokeji <github@hudaokeji.com>
2021-06-18 11:34:32 +0900

    update

--- examples/config.js
+++ examples/config.js
@@ -161,8 +161,8 @@ requirejs.config({
             main: 'main'
          },
          {
-           name : "skylark-domx-plugins",
-           location : "../node_modules/skylark-domx-plugins/dist/uncompressed/skylark-domx-plugins",
+           name : "skylark-domx-plugins-base",
+           location : "../node_modules/skylark-domx-plugins-base/dist/uncompressed/skylark-domx-plugins-base",
             main: 'main'
          },
          {
@@ -173,6 +173,11 @@ requirejs.config({
          {
            name : "skylark-domx-plugins-popups",
            location : "../node_modules/skylark-domx-plugins-popups/dist/uncompressed/skylark-domx-plugins-popups",
+            main: 'main'
+         },
+         {
+           name : "skylark-domx-plugins-scrolls",
+           location : "../node_modules/skylark-domx-plugins-scrolls/dist/uncompressed/skylark-domx-plugins-scrolls",
             main: 'main'
          },
          {
